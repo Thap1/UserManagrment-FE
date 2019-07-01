638f69d1d3a06053871f1d664d94d8e02e0cba50
hxthap <hxthap@cmc.com.vn>
2019-07-01 17:28:59 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationModule } from './authentication/authentication.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ()=>AuthenticationModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,9 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { PmComponent } from './pm/pm.component';
 
 @NgModule({
-  declarations: [AdminComponent, LoginComponent],
+  declarations: [AdminComponent, LoginComponent, RegisterComponent, HomeComponent, UserComponent, PmComponent],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
